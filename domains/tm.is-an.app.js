91710a95fee0d4267bbd1e6e24da7b3efab2d7e6
Andy Cui <andy.cui@ocado.com>
2024-07-27 23:05:05 -0400

    Create tm.is-an.app

---
+++ domains/tm.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'Tesla Mate App',
+  domain: 'is-an.app',
+  subdomain: 'tm',
+  owner: {
+    email: 'andy135790@gmail.com',
+  },
+  record: {
+    A: [
+      '34.72.126.70',
+    ],
+  },
+})
